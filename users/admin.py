3577f01923d049de6a9ff83b3c692af3ea914417
hossainchisty <hossain.chisty11@gmail.com>
2025-10-31 10:44:57 +0600

    feat: Added user model in admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from users.models import User 
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_active', 'date_joined')
+    search_fields = ('username', 'email')
+    list_filter = ('is_active', 'is_staff', 'date_joined')
+    ordering = ('-date_joined',)
