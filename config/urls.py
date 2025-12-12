b9296f9b863fbca97e625f9a5927ae1676bc3cce
hossainchisty <hossain.chisty11@gmail.com>
2025-12-12 18:51:35 +0600

    feat: Added swagger docs url

--- config/urls.py
+++ config/urls.py
@@ -2,21 +2,22 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 from rest_framework import permissions
 
 from config.health import HealthCheckView
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Snippets API",
-        default_version="v1",
-        description="Test description",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="hossain.chisty11@gmail.com"),
-        license=openapi.License(name="Apache-2.0 license"),
-    ),
+schema_view = SpectacularAPIView(
+    title="Snippets API",
+    default_version="v1",
+    description="Test description",
+    terms_of_service="https://www.google.com/policies/terms/",
+    contact=openapi.Contact(email="hossain.chisty11@gmail.com"),
+    license=openapi.License(name="Apache-2.0 license"),
     public=True,
     permission_classes=(permissions.AllowAny,),
 )
@@ -25,15 +26,14 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("health/", HealthCheckView.as_view(), name="health"),
     path("admin/", admin.site.urls),
-    path(
-        "swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"
-    ),
+    # API Documentation
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "swagger/",
-        schema_view.with_ui("swagger", cache_timeout=0),
-        name="schema-swagger-ui",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
     ),
-    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path("redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
     path("api/v1/", include("properties.urls")),
     # user urls
     path("api/v1/users/", include("users.urls")),
